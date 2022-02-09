cfdca754c7544384c71e16c4d3aab0a844b72ae6
Tricia Brooker <grimmi5894@gmail.com>
2022-02-08 20:35:12 -0500

    installed dependencies for testing, mysql2, sequelize, body-parser. updated index.js with planned app.get routes and app.post, app.use routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -9,6 +10,16 @@ app.use(express.static('public'))
 app.get('/', (request, response) => {
   return response.render('index')
 })
+
+app.get('/games', () => {})
+
+app.get('/games:title', () => {})
+
+app.get('games/:system', () => {})
+
+app.use(bodyParser.json())
+
+app.post('/games', () => {})
 
 app.all('*', (request, response) => {
   return response.sendStatus(404)
