0c1bb621ca6391bc8ef8deedee9883498d573b7b
Tricia Brooker <grimmi5894@gmail.com>
2022-02-24 19:12:32 -0500

    added try/catch blocks around all async functions to catch errors with database calls

--- controllers/systems.js
+++ controllers/systems.js
@@ -1,9 +1,13 @@
 const models = require('../models')
 
 const getAllSystems = async (request, response) => {
-  const systems = await models.Systems.findAll()
+  try {
+    const systems = await models.Systems.findAll()
 
-  return response.send(systems)
+    return response.send(systems)
+  } catch (error) {
+    return response.status(500).send('Unable to retrieve systems, please try again')
+  }
 }
 
 module.exports = getAllSystems
