ceba70ea9d3936be2fa8c7601b91a758561b8ec7
Tricia Brooker <grimmi5894@gmail.com>
2022-02-27 08:33:14 -0500

    updated index.js to set server to work with front-end, updated routes for ap.i

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 const express = require('express')
+const cors = require('cors')
+const path = require('path')
 const bodyParser = require('body-parser')
 const { getAllGames, getGameByIdentifier, saveNewGame } = require('./controllers/games')
 const getAllSystems = require('./controllers/systems')
@@ -7,23 +9,26 @@ const getAllSystems = require('./controllers/systems')
 const app = express()
 
 app.set('view engine', 'pug')
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(cors())
+app.use(express.static('client/build'))
 
-app.get('/', (request, response) => {
+app.get('/api', (request, response) => {
   return response.render('index')
 })
 
-app.get('/systems', getAllSystems)
+app.get('/api/systems', getAllSystems)
 
-app.get('/games', getAllGames)
+app.get('/api/games', getAllGames)
 
-app.get('/games/:identifier', getGameByIdentifier)
+app.get('/api/games/:identifier', getGameByIdentifier)
 
-app.post('/games', bodyParser.json(), saveNewGame)
+app.post('/api/games', bodyParser.json(), saveNewGame)
 
-app.all('*', (request, response) => {
+/* app.all('*', (request, response) => {
   return response.sendStatus(404)
-})
+})*/
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client/build', 'index.html')))
 
 app.listen(1337, () => {
   console.log('Listening on port 1337')
